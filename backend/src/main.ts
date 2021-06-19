54da9f7fa395348c903a8cc7160fa1ddd7d3bc89
Ilya Nidzelsky <inidzelsky@gmail.com>
2021-06-19 14:46:07 +0300

    create infrastructure module. add configuration service

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { config } from 'dotenv';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  config({ path: '.env' });
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
