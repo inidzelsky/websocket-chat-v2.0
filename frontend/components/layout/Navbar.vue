234654987a419704c109e947436803c6388e8e7f
Ilya Nidzelsky <inidzelsky@gmail.com>
2021-06-19 19:44:24 +0300

    set up Navbar component

---
+++ frontend/components/layout/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar">
+    <span class="navbar__title">Chat bots 2.0</span>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  position: relative;
+  height: 80px;
+  background-color: #fff;
+
+  &__title {
+    position: absolute;
+    color: #333;
+    font-size: 20pt;
+    font-family: sans-serif;
+    transform: translate(80px, 36px);
+  }
+}
+</style>
