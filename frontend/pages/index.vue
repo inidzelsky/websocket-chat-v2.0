479ef493cf46af74796ff88daba29961cc41d3a2
Ilya Nidzelsky <inidzelsky@gmail.com>
2021-06-24 08:49:02 +0300

    move socket handlers to index from socket plugin

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -6,5 +6,72 @@
 </template>
 
 <script>
-export default {}
+export default {
+  beforeMount() {
+    // Set up socket event handlers
+    // User information
+    this.$socket.on('user', (user) => {
+      const { username } = user
+      localStorage.setItem('username', username)
+
+      this.$store.commit({
+        type: 'user/setUsername',
+        username,
+      })
+    })
+
+    // Available interlocutors
+    this.$socket.on('interlocutors', (users) => {
+      this.$store.commit({
+        type: 'interlocutor/loadUsers',
+        users,
+      })
+    })
+
+    // Bots
+    this.$socket.on('bots', (bots) => {
+      this.$store.commit({
+        type: 'interlocutor/loadBots',
+        bots,
+      })
+    })
+
+    // New interlocutor connected
+    this.$socket.on('interlocutor_connected', (user) => {
+      this.$store.commit({
+        type: 'interlocutor/addUser',
+        user,
+      })
+    })
+
+    // Interlocutor disconnected
+    this.$socket.on('interlocutor_disconnected', (user) => {
+      this.$store.commit({
+        type: 'interlocutor/setOffline',
+        username: user.username,
+      })
+    })
+
+    // User's messages
+    this.$socket.on('messages', (messages) => {
+      const parsedMessages = messages.map((message) => ({
+        ...message,
+        sentAt: new Date(message.sentAt),
+      }))
+
+      this.$store.commit({
+        type: 'message/loadMessages',
+        messages: parsedMessages,
+      })
+    })
+
+    // New message
+    this.$socket.on('message', (message) => {
+      this.$store.commit({
+        type: 'message/addMessage',
+        message: { ...message, sentAt: new Date(message.sentAt) },
+      })
+    })
+  },
+}
 </script>
--- frontend/plugins/socket.js
+++ frontend/plugins/socket.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client'
 import config from '../config'
 
-export default ({ store }, inject) => {
+export default (_, inject) => {
   const { socketServerHost, socketServerPort } = config
   const url = `${socketServerHost}:${socketServerPort}`
 
@@ -9,70 +9,6 @@ export default ({ store }, inject) => {
     query: {
       username: localStorage.getItem('username'),
     },
-  })
-
-  // User information
-  socket.on('user', (user) => {
-    const { username } = user
-    localStorage.setItem('username', username)
-
-    store.commit({
-      type: 'user/setUsername',
-      username,
-    })
-  })
-
-  // Available interlocutors
-  socket.on('interlocutors', (users) => {
-    store.commit({
-      type: 'interlocutor/loadUsers',
-      users,
-    })
-  })
-
-  // Bots
-  socket.on('bots', (bots) => {
-    store.commit({
-      type: 'interlocutor/loadBots',
-      bots,
-    })
-  })
-
-  // New interlocutor connected
-  socket.on('interlocutor_connected', (user) => {
-    store.commit({
-      type: 'interlocutor/addUser',
-      user,
-    })
-  })
-
-  // Interlocutor disconnected
-  socket.on('interlocutor_disconnected', (user) => {
-    store.commit({
-      type: 'interlocutor/setOffline',
-      username: user.username,
-    })
-  })
-
-  // User's messages
-  socket.on('messages', (messages) => {
-    const parsedMessages = messages.map((message) => ({
-      ...message,
-      sentAt: new Date(message.sentAt),
-    }))
-
-    store.commit({
-      type: 'message/loadMessages',
-      messages: parsedMessages,
-    })
-  })
-
-  // New message
-  socket.on('message', (message) => {
-    store.commit({
-      type: 'message/addMessage',
-      message: { ...message, sentAt: new Date(message.sentAt) },
-    })
   })
 
   // Make socket object available for every component to emit events
